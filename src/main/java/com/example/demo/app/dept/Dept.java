253be0a2a482fc4a1dae8c12ecc3296bd1c4118f
ts1013 <developst727@gmail.com>
2020-10-04 16:37:03 +0900

    gettersetter追加、資源パス修正

--- src/main/java/com/example/demo/app/dept/Dept.java
+++ src/main/java/com/example/demo/app/dept/Dept.java
@@ -11,5 +11,42 @@ public class Dept {
  	private String recruitee;
  	private String skills;
  	private String contact;
+ 	
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getRecruitment() {
+		return recruitment;
+	}
+	public void setRecruitment(String recruitment) {
+		this.recruitment = recruitment;
+	}
+	public String getRecruitee() {
+		return recruitee;
+	}
+	public void setRecruitee(String recruitee) {
+		this.recruitee = recruitee;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 
 }
